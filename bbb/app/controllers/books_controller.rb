619b8d39104fcee65301e52d4f72bc63eebd6521
andy-dev <andres.macedo@gmail.com>
2015-07-16 07:48:08 -0700

    add migration to create association of users and books, change controller and models

--- bbb/app/controllers/books_controller.rb
+++ bbb/app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /books
   # GET /books.json
@@ -14,7 +15,9 @@ class BooksController < ApplicationController
 
   # GET /books/new
   def new
-    @book = Book.new
+    #@book = Book.new
+    # we need to build it out from current user
+    @book = current_user.books.build
   end
 
   # GET /books/1/edit
@@ -24,7 +27,7 @@ class BooksController < ApplicationController
   # POST /books
   # POST /books.json
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.build(book_params)
 
     respond_to do |format|
       if @book.save
