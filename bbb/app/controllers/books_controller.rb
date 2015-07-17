289b8caf7d95adbfac76a65dd467c53c79467c3c
andy-dev <andres.macedo@gmail.com>
2015-07-17 16:30:34 -0700

    fix minor typo to show average review

--- bbb/app/controllers/books_controller.rb
+++ bbb/app/controllers/books_controller.rb
@@ -13,7 +13,7 @@ class BooksController < ApplicationController
   def show
     @reviews = Review.where(book_id: @book.id).order("created_at DESC")
 
-    if @review.blank?
+    if @reviews.blank?
       @avg_review = 0
     else
       @avg_review = @reviews.average(:rating).round(2)
