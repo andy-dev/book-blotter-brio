d91851c645365d8f1b72b4dcb93831d7a69ddc5c
andy-dev <andres.macedo@gmail.com>
2015-07-17 15:46:00 -0700

    add association and nested routes for reviews and books, add some styling

--- bbb/app/controllers/reviews_controller.rb
+++ bbb/app/controllers/reviews_controller.rb
@@ -1,31 +1,24 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_book
   before_action :authenticate_user!
-
-  def index
-    @reviews = Review.all
-  end
-
-  def show
-  end
 
 
   def new
     @review = Review.new
   end
 
-
   def edit
   end
-
 
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.book_id = @book.id
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to @book, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -33,7 +26,6 @@ class ReviewsController < ApplicationController
       end
     end
   end
-
 
   def update
     respond_to do |format|
@@ -47,7 +39,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-
   def destroy
     @review.destroy
     respond_to do |format|
@@ -59,6 +50,10 @@ class ReviewsController < ApplicationController
   private
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_book
+      @book = Book.find(params[:book_id])
     end
 
     def review_params
--- bbb/config/routes.rb
+++ bbb/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books
-  resources :reviews
+
+  resources :books do
+    resources :reviews, except: [:show, :index]
+  end
 
   root 'books#index'
 end
